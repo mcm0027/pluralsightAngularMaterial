11309d02726dce9bf2e499b061872f38f45a14dd
Matt Morris <mcm0027@yahoo.com>
2016-02-27 12:55:24 -0600

    add user service and user model

---
+++ app/dist/services/userService.js
@@ -0,0 +1,2 @@
+/// <reference path="../_all.ts" />
+//# sourceMappingURL=userService.js.map
---
+++ app/src/services/userService.ts
@@ -0,0 +1,53 @@
+/// <reference path="../_all.ts" />
+
+module ContactManagerApp {
+
+  export interface IUserService {
+    loadAllUsers(): ng.IPromise<User[]>;
+    selectedUser: User;
+  }
+
+  export class UserService implements IUserService {
+    static $inject = ['$q'];
+
+    constructor(private $q: ng.IQService) {
+    }
+
+    selectedUser: User = null;
+
+    loadAllUsers() : ng.IPromise<User[]> {
+      return this.$q.when(this.users);
+    }
+
+    private users: User[] = [
+      {
+        name: 'Erick Riley',
+        avatar: 'svg-1',
+        bio: "fish fish blah doob",
+        notes: [
+          { title: "Pay back dinner", date: new Date("2016-01-12")},
+          { title: "Buy flowers for birthday", date: new Date("2016-01-19")}
+        ]
+      },
+         {
+        name: 'Erick Riley',
+        avatar: 'svg-2',
+        bio: "quick squip doob kappa",
+        notes: [
+          { title: "Pay back chipmunk", date: new Date("2016-01-12")},
+          { title: "Buy whips for birthday", date: new Date("2016-01-19")}
+        ]
+      },
+         {
+        name: 'Erick Riley',
+        avatar: 'svg-3',
+        bio: "blah blah blah doob",
+        notes: [
+          { title: "Pay back choco", date: new Date("2016-01-12")},
+          { title: "Buy smells for birthday", date: new Date("2016-01-19")}
+        ]
+      }
+    ]
+  }
+
+}
